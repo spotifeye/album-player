52940ca5a8d186971defa5174b2caa79edf918c5
David Hong <davydhong@gmail.com>
2018-09-26 16:59:23 -0700

    setup filesfor modular routing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import Album from './Album.jsx';
-import Player from './Player.jsx'; 
+import Player from './Player.jsx';
 import appCss from '../css_components/App.css';
 
 class App extends React.Component {
@@ -11,10 +11,10 @@ class App extends React.Component {
       artist: {},
       artistID: 0,
       artistName: '',
-      albums: [{albumImage: "", songs: [{songName: ""}]}],
+      albums: [{ albumImage: '', songs: [{ songName: '' }] }],
       albumPlayingID: 0,
       songPlayingID: 0
-    }
+    };
     var artistId = Math.floor(Math.random() * 100) + 1;
     this.getAllArtists(artistId);
   }
@@ -26,17 +26,17 @@ class App extends React.Component {
 
   getAllArtists(artistId) {
     $.ajax({
-      method: "GET",
-      url: "/artists/albums/" + artistId,
-      success: (data) => {
+      method: 'GET',
+      url: `/artists/${artistId}/albums/`,
+      success: data => {
         this.setState({
           artist: data[0],
           artistID: artistId,
           artistName: data[0].artistName,
           albums: data[0].albums
-        })
+        });
       }
-    })
+    });
   }
 
   updateAlbumSongPlaying(albumID, songID) {
@@ -51,31 +51,26 @@ class App extends React.Component {
     for (var i = 0; i < this.state.albums.length; i++) {
       albums.push(
         <div>
-          <Album album={this.state.albums[i]} id={i+1} update={this.updateAlbumSongPlaying.bind(this)} 
-                albumPlaying={this.state.albumPlayingID} songPlayingID={this.state.songPlayingID}/>
+          <Album album={this.state.albums[i]} id={i + 1} update={this.updateAlbumSongPlaying.bind(this)} albumPlaying={this.state.albumPlayingID} songPlayingID={this.state.songPlayingID} />
         </div>
-      )
+      );
     }
     return albums;
   }
 
   render() {
-    return(
+    return (
       <div>
         <div className={appCss.albumsModule}>
           <h3 id={appCss.AppAlbumTitle}>Albums</h3>
-          <hr id={appCss.horizLine}/>
-          <div>
-            {this.buildAlbums()}
-          </div>
+          <hr id={appCss.horizLine} />
+          <div>{this.buildAlbums()}</div>
         </div>
-        <div className={appCss.playerModule}><Player artist={this.state.artistName} 
-                                               albums={this.state.albums} 
-                                               albumPlaying={this.state.albumPlayingID}
-                                               songPlaying={this.state.songPlayingID}
-                                               updateAlbumSongPlaying={this.updateAlbumSongPlaying.bind(this)}/></div>
+        <div className={appCss.playerModule}>
+          <Player artist={this.state.artistName} albums={this.state.albums} albumPlaying={this.state.albumPlayingID} songPlaying={this.state.songPlayingID} updateAlbumSongPlaying={this.updateAlbumSongPlaying.bind(this)} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,32 @@ const path = require('path');
 const { getArtist } = require('../database/index.js');
 const cors = require('cors');
 const server = express();
+const compression = require('compression');
 
 server.use(bodyParser.json());
 server.use(cors());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
-server.get('/artists/albums/:artistID', (req, res) => {
+server.get('/artists/:artistID/albums/', (req, res) => {
+  getArtist(req.params.artistID, data => {
+    res.send(data);
+  });
+});
+
+server.post('/artists/:artistID/albums/', (req, res) => {
+  getArtist(req.params.artistID, data => {
+    res.send(data);
+  });
+});
+
+server.put('/artists/:artistID/albums/', (req, res) => {
+  getArtist(req.params.artistID, data => {
+    res.send(data);
+  });
+});
+
+server.delete('/artists/:artistID/albums/', (req, res) => {
   getArtist(req.params.artistID, data => {
     res.send(data);
   });
